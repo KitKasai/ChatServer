70a5e128a2a3c5cbafa6c5619d6540670ee12dd2
Christine <shawnshengss@gmail.com>
2018-05-29 15:56:34 -0700

    actually fixed client websocket connection this time

--- client/client.js
+++ client/client.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         const hostname = config.hostname;
         const port = config.port;
 
-        const socket = new WebSocket(`ws://${document.domain}:${port}/`);
+        const socket = new WebSocket(`ws://${document.domain}/`);
         socket.addEventListener('open', onOpen(socket));
         socket.addEventListener('message', onMessage(socket));
     });
